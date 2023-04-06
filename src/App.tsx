d2847a1eea35361c7013bc2eb22f9403e2342ab2
Hardik Sachan <sachanhardik@gmail.com>
2023-04-06 17:01:24 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="main">
+      <h1 className="main__heading">Template</h1>
+      <ul className="main__list">
+        <li className="main__item">React</li>
+        <li className="main__item">TypeScript</li>
+        <li className="main__item">Lint - eslint & prettier</li>
+        <li className="main__item">Husky</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
